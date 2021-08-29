2229c24043fa335ab12a5f2a46aad4512f0af887
Bryan Medina <bmedinacordero2@gmail.com>
2021-08-29 03:57:51 -0400

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,4 +21,46 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
-//template literal
+let lorem = "Quiero escribir una frase epica que podamos separar muy profesionalmente \n"
++ "Aqui va otra frase epica que necesitamos." 
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    "name": "Oscar",
+    "age": 32,
+    "country": "MX"
+}
+
+console.log(person.name,person.age,person.country);
+
+let {name, age} = person;
+console.log(name,age);
+
+let team1 = ["Oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Jessica", "Camila"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(education);
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = "Global Let";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+// console.log(globalLet);
+
+const a = "b";
+a = "a";
+console.log(a);

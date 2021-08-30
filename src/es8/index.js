19f520598ea980ac5c20a2b9cccefab5f159893b
Bryan Medina <bmedinacordero2@gmail.com>
2021-08-30 01:29:54 -0400

    ¿Qué se implementó en ES8?

---
+++ src/es8/index.js
@@ -0,0 +1,27 @@
+const data = {
+    frontend: "Bryan",
+    backend: "Emanuel",
+    design: "Ana",
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const data = {
+    frontend: "Bryan",
+    backend: "Emanuel",
+    design: "Ana",
+}
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+
+const string = "Hello";
+console.log(string.padStart(7, "hi"));
+console.log(string.padEnd(12, " -----"));
+
+const obj = {
+    name: "Oscar",
+}

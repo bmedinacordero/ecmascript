6880e194ae0c90f1a15d68f53ad9503f664bfd63
Bryan Medina <bmedinacordero2@gmail.com>
2021-08-31 01:07:51 -0400

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -25,3 +25,32 @@ console.log(string.padEnd(12, " -----"));
 const obj = {
     name: "Oscar",
 }
+
+//Async Await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+        ? setTimeout(() => resolve ("Hello World"), 3000)
+        : reject(new Error ("Test Error"))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
